c1d956d37f8a29be25fe3f7d4b80df814c2c9ec4
stiartsly <stiartsly@gmail.com>
2021-07-30 14:08:30 +0800

    merge the logic implementation of upgrade contracts library and command

--- tools/upgrade.js
+++ tools/upgrade.js
@@ -1,49 +1,86 @@
-const expect = require("chai").expect;
+const { getWeb3, getAccount, sendTxWaitForReceipt } = require("./utils");
 const { getParams } = require("./upgrade_params");
-const { upgradeContracts  } = require("./upgrade_contracts");
+const { proxiableABI } = require("./upgrade_config");
+const { expect } = require("chai");
 
-(async () => {
+const upgradeContracts = async (owner, proxyAddr, newCodeAddr, gasPrice, rpcUrl = null) => {
   try {
-    console.log("=== Start to upgrade contracts.");
-    const {
-      ownerPK, 
+    const web3 = await getWeb3(rpcUrl);
+    const proxyContract = new web3.eth.Contract(proxiableABI, proxyAddr);
+
+    const accOwner = await getAccount(owner);
+
+    const upgradeData = proxyContract.methods.updateCodeAddress(newCodeAddr).encodeABI();
+    const upgradeTx = {
+      from: accOwner.address,
+      to: proxyAddr,
+      value: 0,
+      data: upgradeData,
       gasPrice,
-      rpcUrl,
-      proxiedNftAddr,
-      newNftAddr,
-      proxiedPasarAddr,
-      newPasarAddr } = await getParams();
-    
-    console.log("gasPrice: ", gasPrice);
-    console.log("rpcUrl:", rpcUrl);
-    console.log("ProxiedNftAddr: ", proxiedNftAddr);
-    console.log("newNftAddr: ", newNftAddr);
-    console.log("ProxiedPasarAddr: ", proxiedPasarAddr);
-    console.log("NewPasarAddr: ", newPasarAddr);
-    console.log("");
+    };
 
-    if (proxiedNftAddr && newNftAddr) {
-      const result = await upgradeContracts(ownerPK, proxiedNftAddr, newNftAddr, gasPrice, rpcUrl);
-      expect(result, "Result of upgrading logic NFT contract ").to.equal(true);
-      console.log("")
-      console.log("Logic NFT contract successfully has been upgraded");
-    } else {
-      console.log("No need to upgrade for logic NFT contract");
+    const { status: upgradeStatus } = await sendTxWaitForReceipt(upgradeTx, accOwner);
+    if (!upgradeStatus) {
+      console.error(`Upgrade logic contract for proxy contract ${proxyAddr} transaction failed`);
+      return false;
     }
-
-    if (proxiedPasarAddr && newPasarAddr) {
-      console.log("=== upgrade logic Pasar contract");
-      const result = await upgradeContracts(ownerPK, proxiedPasarAddr, newPasarAddr, gasPrice, rpcUrl);
-      expect(result, "Result of upgrading logic Pasar contract ").to.equal(true);
-      console.log("Logic Pasar contract successfully has been upgraded");
-    }
-    else {
-      console.log("No need to upgrade for logic Pasar contract");
-    }
-
-    console.log("=== Upgrade contracts finished ===");
+    console.info(`Logic contract upgraded to ${newCodeAddr} for proxy contract ${proxyAddr} successfully`);
+    return true;
   } catch (err) {
     console.error(String(err));
-    console.error("=== Upgrade contracts failed ===");
+    return false;
   }
-})();
+};
+
+module.exports = {
+  upgradeContracts,
+};
+
+if (require.main == module) {
+  (async () => {
+    try {
+      console.log("=== Start to upgrade contracts.");
+      const {
+        ownerPK,
+        gasPrice,
+        rpcUrl,
+        proxiedNftAddr,
+        newNftAddr,
+        proxiedPasarAddr,
+        newPasarAddr } = await getParams();
+
+      console.log("gasPrice       :", gasPrice);
+      console.log("rpcUrl         :", rpcUrl);
+      console.log("ProxiedNftAddr :", proxiedNftAddr);
+      console.log("newNftAddr     :", newNftAddr);
+      console.log("ProxiedPasarAddr:", proxiedPasarAddr);
+      console.log("NewPasarAddr   :", newPasarAddr);
+      console.log("");
+
+      if (proxiedNftAddr && newNftAddr) {
+        console.log("=== upgrade logic NFT contract");
+        const result = await upgradeContracts(ownerPK, proxiedNftAddr, newNftAddr, gasPrice, rpcUrl);
+        console.log(`result: ${result}`);
+        expect(result, "Result of upgrading logic NFT contract ").to.equal(true);
+        console.log("Logic NFT contract successfully has been upgraded");
+      } else {
+        console.log("No need to upgrade for logic NFT contract");
+      }
+
+      if (proxiedPasarAddr && newPasarAddr) {
+        console.log("=== upgrade logic Pasar contract");
+        const result = await upgradeContracts(ownerPK, proxiedPasarAddr, newPasarAddr, gasPrice, rpcUrl);
+        expect(result, "Result of upgrading logic Pasar contract ").to.equal(true);
+        console.log("Logic Pasar contract successfully has been upgraded");
+      }
+      else {
+        console.log("No need to upgrade for logic Pasar contract");
+      }
+
+      console.log("=== Upgrade contracts finished ===");
+    } catch (err) {
+      console.error(String(err));
+      console.error("=== Upgrade contracts failed ===");
+    }
+  })();
+}
--- tools/upgrade_config.js
+++ tools/upgrade_config.js
@@ -20,69 +20,6 @@ module.exports = {
   // the new logic Pasar address to replace with the old one
   newPasarAddr: "",
 
-  // demo ABI should not be modified
-  demoABI: [
-    {
-      "inputs": [],
-      "name": "getA",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "getB",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "initialize",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_a",
-          "type": "uint256"
-        }
-      ],
-      "name": "setA",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_b",
-          "type": "uint256"
-        }
-      ],
-      "name": "setB",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ],
-
   proxiableABI: [
     {
       "inputs": [],
