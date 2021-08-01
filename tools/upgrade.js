f05de0aba8c2e4c4b638e6f8d6a760ddaafc590e
stiartsly <stiartsly@gmail.com>
2021-08-01 15:28:51 +0800

    Added code to check whether the ugprade is successful or not

--- tools/upgrade.js
+++ tools/upgrade.js
@@ -1,11 +1,11 @@
-const { getWeb3, getAccount, sendTxWaitForReceipt } = require("./utils");
+const { loadFile, getWeb3, getAccount, sendTxWaitForReceipt } = require("./utils");
 const { getParams } = require("./upgrade_params");
 const { proxiableABI } = require("./upgrade_config");
 const { expect } = require("chai");
 
 const upgradeContracts = async (owner, proxyAddr, newCodeAddr, gasPrice, rpcUrl = null) => {
   try {
-    const web3 = await getWeb3(rpcUrl);
+    const web3 = await getWeb3();
     const proxyContract = new web3.eth.Contract(proxiableABI, proxyAddr);
 
     const accOwner = await getAccount(owner);
@@ -25,6 +25,10 @@ const upgradeContracts = async (owner, proxyAddr, newCodeAddr, gasPrice, rpcUrl 
       return false;
     }
     console.info(`Logic contract upgraded to ${newCodeAddr} for proxy contract ${proxyAddr} successfully`);
+
+    const newAddr = await proxyContract.methods.getCodeAddress().call();
+    expect(newAddr, "New code address after upgraded").to.equal(newCodeAddr);
+
     return true;
   } catch (err) {
     console.error(String(err));
@@ -59,19 +63,61 @@ if (require.main == module) {
 
       if (proxiedNftAddr && newNftAddr) {
         console.log("=== upgrade logic NFT contract");
+
+        const jsonStr = await loadFile("../abis/FeedsNFTSticker.json");
+        const nftABI = JSON.parse(jsonStr);
+
+        const web3 = await getWeb3(rpcUrl);
+        const nftContract = new web3.eth.Contract(nftABI, proxiedNftAddr);
+        console.log("NFT contract object initialized");
+
+        /*
+        const oldVersion = await nftContract.methods.getVersion().call();
+        console.log(`The original version of NFT contract is ${oldVersion}`);
+        expect(oldVersion, "The original version value").to.equal("v0.1")
+
+        const oldMagic = await nftContract.methods.getMagic().call();
+        console.log(`The orignal magic value of NFT contract is ${oldMagic}`);
+        expect(oldMagic, "The original magic value").to.equal("v0.1");
+        */
+
         const result = await upgradeContracts(ownerPK, proxiedNftAddr, newNftAddr, gasPrice, rpcUrl);
         console.log(`result: ${result}`);
         expect(result, "Result of upgrading logic NFT contract ").to.equal(true);
         console.log("Logic NFT contract successfully has been upgraded");
+
+        const newVersion = await nftContract.methods.getVersion().call();
+        console.log(`The new version of NFT contract is ${newVersion}`);
+        expect(newVersion, "The new version value").to.equal("v0.2")
+
+        const newMagic = await nftContract.methods.getMagic().call();
+        console.log(`The new magic value of NFT contract is ${newMagic}`);
+        expect(newMagic, "The new magic value").to.equal("20210801")
       } else {
         console.log("No need to upgrade for logic NFT contract");
       }
 
       if (proxiedPasarAddr && newPasarAddr) {
         console.log("=== upgrade logic Pasar contract");
+
+        const jsonStr = await loadFile("../abis/FeedsNFTPasar.json");
+        const pasarABI = JSON.parse(jsonStr);
+
+        const web3 = await getWeb3(rpcUrl);
+        const pasarContract = new web3.eth.Contract(pasarABI, proxiedPasarAddr);
+        console.log("NFT contract object initialized");
+
         const result = await upgradeContracts(ownerPK, proxiedPasarAddr, newPasarAddr, gasPrice, rpcUrl);
         expect(result, "Result of upgrading logic Pasar contract ").to.equal(true);
         console.log("Logic Pasar contract successfully has been upgraded");
+
+        const newVersion = await pasarContract.methods.getVersion().call();
+        console.log(`The new version of Pasar contract is ${newVersion}`);
+        expect(newVersion, "The new version value").to.equal("v0.2")
+
+        const newMagic = await pasarContract.methods.getMagic().call();
+        console.log(`The new magic value of Pasar contract is ${newMagic}`);
+        expect(newMagic, "The new magic value").to.equal("20210801")
       }
       else {
         console.log("No need to upgrade for logic Pasar contract");
