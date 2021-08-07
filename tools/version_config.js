80e2b3227a8f4d3d52e80fd86a34e348543eec18
stiartsly <stiartsly@gmail.com>
2021-08-07 21:38:11 +0800

    Added the tool to get version/magic of the specific contract according to address

---
+++ tools/version_config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // ethereum node RPC URL
+  rpcUrl: "https://api-testnet.trinity-tech.cn/eth",
+  
+  // the address of NFT contract
+  nftAddr: "",
+
+  // the address of Pasar contract
+  pasarAddr: "",
+};
+
---
+++ tools/version_params.js
@@ -0,0 +1,23 @@
+const { Command } = require("commander");
+const config = require("./version_config");
+
+const getParams = async () => {
+  try {
+    const program = new Command();
+    program.version("1.0.0");
+    program
+      .option("--rpcUrl <url>", "eth smartcontract sidechain rpc url", config.rpcUrl)
+      .option("--nftAddr <address>", "the address of NFT contract", config.nftAddr)
+      .option("--pasarAddr <address>", "the address of Pasar contract", config.pasarAddr);
+    program.parse();
+    const options = program.opts();
+    return options;
+  } catch (err) {
+    console.error(String(err));
+    return;
+  }
+};
+
+module.exports = {
+  getParams
+};
