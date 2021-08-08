1b6f557a752ec5c148e8ed1da75c06cb1a54c02d
stiartsly <stiartsly@gmail.com>
2021-08-08 13:43:30 +0800

    Update the tool deploy.js that can deploy the single contract or all contracts

--- tools/deploy_params.js
+++ tools/deploy_params.js
@@ -9,6 +9,9 @@ const getParams = async () => {
       .option("--rpcUrl <url>", "eth smartcontract sidechain rpc url", config.rpcUrl)
       .option("--gasPrice <price>", "gas price", config.gasPrice)
       .option("--deployPK <key>", "private key of account to deploy the contracts", config.deployPK)
+      .option("--withNFT <bool>", "with NFT contract to deploy", config.withNFT)
+      .option("--withPasar <bool>", "with Pasar contract to deploy", config.withPasar)
+      .option("--withProxy <bool>", "with Proxied contracts to deploy", config.withProxy);
     program.parse();
     const options = program.opts();
     return options;
