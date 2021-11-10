253a53dfce97da65f032b3b8b140a6db280b4812
stiartsly <stiartsly@gmail.com>
2021-11-10 18:31:21 +0800

    CU-1rc44rt - Added to get logic contract address with proxied contract address

--- tools/version_config.js
+++ tools/version_config.js
@@ -1,11 +1,40 @@
 module.exports = {
   // ethereum node RPC URL
-  rpcUrl: "https://api-testnet.trinity-tech.cn/eth",
+  rpcUrl: "",
   
-  // the address of NFT contract
+  // the address of NFT contract (must be proxied address)
   nftAddr: "",
 
-  // the address of Pasar contract
+  // the address of Pasar contract (must be proxied address)
   pasarAddr: "",
+
+  proxiableABI: [
+    {
+      "inputs": [],
+      "name": "getCodeAddress",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_newAddress",
+          "type": "address"
+        }
+      ],
+      "name": "updateCodeAddress",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
 };
 
