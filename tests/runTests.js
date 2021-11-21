75181671cb5d29527c51368cd5dab9d6c4f884e0
macdev <macdev@dev.mac>
2021-11-21 15:20:58 +0800

    Galleria contract tests
    add tests for FeedsNFTGalleria contract

--- tests/runTests.js
+++ tests/runTests.js
@@ -3,6 +3,7 @@ const { getParams, getWeb3 } = require("./utils");
 const { testDeploy } = require("./deployContracts");
 const { testSticker } = require("./testSticker");
 const { testPasar } = require("./testPasar");
+const { testGalleria } = require("./testGalleria");
 
 (async () => {
   try {
@@ -18,13 +19,18 @@ const { testPasar } = require("./testPasar");
     const {
       stickerABI,
       pasarABI,
+      galleriaABI,
       proxyStickerAddr: sticker,
       proxyPasarAddr: pasar,
+      proxyGalleriaAddr: galleria,
     } = await testDeploy(deployerPK, gasPrice);
     console.log("=== Contracts deployed ===");
     const stickerResult = await testSticker(stickerABI, sticker, creatorPK, sellerPK, "42", gasPrice);
     expect(stickerResult, "Sticker token tests result").to.equal(true);
     console.log("=== Sticker token tests complete");
+    const galleriaResult = await testGalleria(galleriaABI, galleria, stickerABI, creatorPK, "42", gasPrice);
+    expect(galleriaResult, "Galleria contract tests result").to.equal(true);
+    console.log("=== Galleria contract tests complete");
     const pasarResult = await testPasar(pasarABI, pasar, stickerABI, creatorPK, sellerPK, buyerPK, bidderPK, "42", gasPrice);
     expect(pasarResult, "Pasar contract tests result").to.equal(true);
     console.log("=== Pasar contract tests complete");
